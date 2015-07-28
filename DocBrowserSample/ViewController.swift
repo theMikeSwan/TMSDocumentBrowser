8420239145faf68bc53634f891ec929d8d15a236
Mike Swan (TARDIS) <mikeswan@theMikeSwan.com>
2015-07-28 16:21:36 -0400

    Initial Commit

---
+++ DocBrowserSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DocBrowserSample
+//
+//  Created by Mike Swan on 7/28/15.
+//  Copyright © 2015 theMikeSwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
